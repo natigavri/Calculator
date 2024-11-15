0361814b324269a08133844e4514c7ff71d6577a
Natigavri <nati661@gmail.com>
2024-11-15 15:14:27 +0200

    Improve CSS, fix bugs, add pos\neg toggle
    improved css.
    fixed issues happening after repeated pressing
    on operators.
    added a positive & negative number toggle.

--- scripts.js
+++ scripts.js
@@ -35,7 +35,7 @@ let currentDisp = NaN;
 let currentOperator = null;
 let numA = NaN;
 let numB = NaN;
-let numATaken = false;
+// let numATaken = false;
 
 /* long decimals formatter */
 const formatter = new Intl.NumberFormat('en-US', {
@@ -44,7 +44,7 @@ const formatter = new Intl.NumberFormat('en-US', {
 });
 
 /* displays pressed number on screen */
-function scrDisplay(userInput){
+function scrnDisplay(userInput){
     let currentNum = display.innerHTML;
     if (!(Number.isInteger(currentDisp))){
         display.innerHTML = userInput;
@@ -59,18 +59,19 @@ function scrDisplay(userInput){
 /* Clear screen and reset saved values */
 function clearScrn() {
     currentDisp = NaN;
+    currentOperator = null;
     numA = NaN;
     numB = NaN;
-    numATaken = false;
+    // numATaken = false;
 }
 
 /* Calculate user inputs */
 function calc(operator, numA, numB) {
     let calculation = NaN;
-    if (operator === "/" && numB === 0){
+    if (operator === "/" && numB === 0){ /* Check if user trying to divide by 0 */
         currentDisp = 'Dont!';
         display.innerHTML = currentDisp;
-        numATaken = false;
+        // numATaken = false;
     }
     else{
         currentDisp = formatter.format(operate(operator, numA, numB));
@@ -88,38 +89,58 @@ function calc(operator, numA, numB) {
 const digits = document.getElementById("digits");
 digits.addEventListener("click", e => {
     if(e.target.className === 'digit'){
-        currentDisp = scrDisplay(parseFloat(e.target.innerHTML));
-        if (numATaken){
+        currentDisp = scrnDisplay(e.target.innerHTML);
+        if (!(isNaN(numA)) && currentOperator !== null){
             numB = parseInt(currentDisp);
         }else{
-            numA = parseInt(currentDisp);
-            
+            numA = parseInt(currentDisp);   
         }   
     }
 });
 /* listen for operator press, save into variable */
 const operators = document.getElementById("operators");
 operators.addEventListener("click", e => {
-    if(e.target.className.includes('operator')){
-        if(numATaken && Number.isInteger(numB)){
-            numA = calc(currentOperator, numA, numB);
-            
+    if(e.target.className.includes('operator')){ /* Check if user pressed on "*, /, +, -" */
+        if(!(isNaN(numA)) && !(isNaN(numB))){
+            numA = calc(currentOperator, numA, numB); 
         }
         currentOperator = e.target.innerHTML;
         currentDisp = NaN;
-        numATaken = true;
-    }else if(e.target.className.includes('clearDisp')){
+        // numATaken = true;
+    }else if(e.target.className.includes('clearDisp')){ /* Check if user pressed on "C" */
         display.innerHTML = '';
         clearScrn();
         
     }
-    else if(e.target.className.includes('calculate')){
+    else if(e.target.className.includes('calculate')){ /* Check if user pressed on "=" */
         if ((Number.isInteger(numB))){
             let eval = calc(currentOperator, numA, numB);
             clearScrn();
             numA = eval;
         }
-        
+    }
+    else if(e.target.className.includes('plusOrMinus')){
+        let disp = parseInt(display.innerHTML)
+        let sign = Math.sign(disp)
+        if (sign !== 0){
+            if (numA === disp) {
+                if (sign === 1){
+                    numA = numA * ((-1) * sign);
+                    display.innerHTML = disp*((-1) * sign);
+                }else{
+                    numA = numA * (-1);
+                    display.innerHTML = disp* (-1);
+                }
+            }else{
+                if (sign === 1){
+                    numB = numB * ((-1) * sign);
+                    display.innerHTML = disp*((-1) * sign);
+                }else{
+                    numB = numB * (-1);
+                    display.innerHTML = disp* (-1);
+                }
+            }
+        }
     }
 
 });
